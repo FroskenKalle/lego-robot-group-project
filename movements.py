db2731176b710e25792335d92cef1751866180d0
memarao <73643667+memarao@users.noreply.github.com>
2020-11-02 13:40:51 +0100

    Create movements.py

---
+++ movements.py
@@ -0,0 +1,29 @@
+#Movement 1 (Following line and checking every seconds)
+
+class FirstRoute1
+    colorVariable = Color()
+    while movements < 100
+        if colorVariable = Color.BLACK:
+            motor_a.run_time(180, 500, Brake.coast wait.false)
+            motor_b.run_time(90, 500)
+
+        elif colorVariable = Color.WHITE:
+            motor_a.run_time(90, 250, Brake.coast wait.false)
+            motor_b.run_time(180, 250)
+        movements += 1
+        pass 
+
+
+#Movement 2 (Following the line and checking after x cm)
+
+class FirstRoute2
+    motor_a.run_time(180, 500, Brake.coast wait.false)
+    motor_b.run_time(180, 500)
+        #Counting how many cm/s it goes with speed 180dgr pr 1/2 sec.
+    pass
+
+#Movement 3 ()
+
+class FirstRoute3
+
+pass
